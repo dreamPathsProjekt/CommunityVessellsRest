ced68b21399cd7f5f805ae024f31ab69d6345ddf
DreamPaths Projekt <dream.paths.projekt@gmail.com>
2017-07-25 02:49:12 +0300

    Implement register Volunteer ajax request

--- src/main/resources/static/js/reg-volntr.js
+++ src/main/resources/static/js/reg-volntr.js
@@ -1,1 +1,49 @@
 var url = 'http://localhost:8080/api/';
+
+var customURI = url + 'volunteer/';
+$(function () {
+  $('#volBtn').click(function (e) {
+    e.preventDefault();
+
+    var email = $('#sendVol #inputEmail').val();
+    var password = $('#sendVol #inputPassword').val();
+    var firstName = $('#sendVol #inputFirstname').val();
+    var lastName = $('#sendVol #inputLastname').val();
+
+    // var formData = new FormData();
+    //*  get the file after ajax request
+    // var files = $("#inputFile").get(0).files;
+    var request =
+    {
+      'email': email,
+      'password': password,
+      'firstName': firstName,
+      'lastName': lastName
+    };
+
+    var data = JSON.stringify(request);
+    console.log(request);
+    alert(request);
+
+    var ajaxRequest = $.ajax({
+      type: 'POST',
+      url: customURI,
+      contentType: 'application/json',
+      processData: true,
+      data: data,
+
+      error: function (xhr, status, error) {
+        console.log(xhr);
+        console.log(status);
+        console.log(error);
+        console.log(request);
+      }
+    });
+
+    ajaxRequest.done(function (xhr, textStatus) {
+    // $('#response').attr('class', 'alert alert-success');
+      alert('Volunteer created successfully');
+    });
+  });
+  return false;
+});
