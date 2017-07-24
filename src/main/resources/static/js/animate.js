7a044152c66ccc885ac538f940df6444dddd0802
DreamPaths Projekt <dream.paths.projekt@gmail.com>
2017-07-24 11:16:22 +0300

    Configure ESLint for jQuery & Browser manually

--- src/main/resources/static/js/animate.js
+++ src/main/resources/static/js/animate.js
@@ -1,17 +1,13 @@
 
 $(document).ready(function () {
-    $(window).scroll(function() {
-      $(".slideanim").each(function(){
-        var pos = $(this).offset().top;
+  $(window).scroll(function () {
+    $('.slideanim').each(function () {
+      var pos = $(this).offset().top;
 
-        var winTop = $(window).scrollTop();
-        if (pos < winTop + 600) {
-          $(this).addClass("slide");
-        }
-      });
+      var winTop = $(window).scrollTop();
+      if (pos < winTop + 600) {
+        $(this).addClass('slide');
+      }
     });
+  });
 });
-
-
-
-
--- src/main/resources/static/js/material_init.js
+++ src/main/resources/static/js/material_init.js
@@ -1,22 +1,22 @@
 $(function () {
-    $.material.init();
-		$.material.ripples();
-    $(".shor").noUiSlider({
-      start: 40,
-      connect: "lower",
-      range: {
-        min: 0,
-        max: 100
-      }
-    });
-    
-    $(".svert").noUiSlider({
-      orientation: "vertical",
-      start: 40,
-      connect: "lower",
-      range: {
-        min: 0,
-        max: 100
-      }
-    });
-});
+  $.material.init()
+  $.material.ripples()
+  $('.shor').noUiSlider({
+    start: 40,
+    connect: 'lower',
+    range: {
+      min: 0,
+      max: 100
+    }
+  })
+
+  $('.svert').noUiSlider({
+    orientation: 'vertical',
+    start: 40,
+    connect: 'lower',
+    range: {
+      min: 0,
+      max: 100
+    }
+  })
+})
